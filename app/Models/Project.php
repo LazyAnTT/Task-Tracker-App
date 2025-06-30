44047ac66d37935606b4b0d38f9aa6457ed2988b
Raimonds Grūbe <raimisgrube@gmail.com>
2025-06-30 06:56:27 +0300

    Created eloquent relationships between project and task

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'description'];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'due_date',
+        'assigned_to',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
